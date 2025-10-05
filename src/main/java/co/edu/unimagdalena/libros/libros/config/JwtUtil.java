f53cd9de924cd123686e611d36b2e00f900422a0
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-10-05 17:45:21 -0500

    mendez lo pidio 2

--- src/main/java/co/edu/unimagdalena/libros/libros/config/JwtUtil.java
+++ src/main/java/co/edu/unimagdalena/libros/libros/config/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class JwtUtil {
@@ -31,6 +32,7 @@ public class JwtUtil {
 
         String token = Jwts.builder()
                 .setSubject(userDetails.getEmail())
+                .claim("UserId", userDetails.getId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
                 .signWith(key(), SignatureAlgorithm.HS512)
@@ -63,13 +65,15 @@ public class JwtUtil {
                 .getSubject();
     }
 
-    public String getIdFromJwtToken(String token) {
-        return Jwts.parserBuilder()
+    public UUID getIdFromJwtToken(String token) {
+        String id = Jwts.parserBuilder()
                 .setSigningKey(key())
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
-                .getId();
+                .get("UserId", String.class);
+
+        return id != null ? java.util.UUID.fromString(id) : null;
     }
 
     @PostConstruct
