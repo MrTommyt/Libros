e4625f9500115cb63b630ea5496d7d976f9bf504
MrTommyt <benavidescamilo199@gmail.com>
2025-10-06 02:38:55 -0500

    fixes to the book controler

--- src/main/java/co/edu/unimagdalena/libros/libros/controller/BookController.java
+++ src/main/java/co/edu/unimagdalena/libros/libros/controller/BookController.java
@@ -1,15 +1,25 @@
 package co.edu.unimagdalena.libros.libros.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.unimagdalena.libros.libros.config.JwtUtil;
 import co.edu.unimagdalena.libros.libros.dto.BookDto;
-import co.edu.unimagdalena.libros.libros.entity.Book;
 import co.edu.unimagdalena.libros.libros.service.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +27,13 @@ import java.util.UUID;
 @CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
     private final BookService bookService;
+    private final JwtUtil jwtUtil;
+
+    public BookController(BookService bookService, JwtUtil jwtUtil) {
+        this.bookService = bookService;
+        this.jwtUtil = jwtUtil;
+    }
+
 
     //esto es para mostrar todos los libros publicados para un home sin login
     @GetMapping("/all")
@@ -50,8 +67,8 @@ public class BookController {
     public ResponseEntity<List<BookDto>> getMyBooks(
             @RequestHeader("Authorization") String authorizationHeader
     ) {
-        String token = authorizationHeader.substring("bearer ".length());
-        UUID clientId = jwtService.extractClientId(token);
+        String token = authorizationHeader.substring("Bearer ".length());
+        UUID clientId = jwtUtil.getIdFromJwtToken(token);
         return ResponseEntity.ok(bookService.findBooksByClientId(clientId));
     }
 
@@ -60,8 +77,8 @@ public class BookController {
     public ResponseEntity<List<BookDto>> getBooksFromOthers(
             @RequestHeader("Authorization") String authorizationHeader
     ) {
-        String token = authorizationHeader.substring("bearer ".length());
-        UUID clientId = jwtService.extractClientId(token);
+        String token = authorizationHeader.substring("Bearer ".length());
+        UUID clientId = jwtUtil.getIdFromJwtToken(token);
         return ResponseEntity.ok(bookService.findBooksFromOthers(clientId));
     }
 
